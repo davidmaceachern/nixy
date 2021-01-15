cec41c137f65ec6aae6d9146e42ee93f714d1c13
David MacEachern <david.maceachern.tech@gmail.com>
2021-01-15 12:07:47 +0100

    Rename

---
+++ shell.nix
@@ -0,0 +1,22 @@
+let
+  sources = import ./nix/sources.nix;
+  pkgs = import sources.nixpkgs {};
+in
+pkgs.mkShell {
+  name = "aws-node";
+
+  buildInputs = [
+    pkgs.nodejs-12_x
+    pkgs.bashInteractive
+    pkgs.jq
+  ];
+
+  shellHook = ''
+    export PATH="$PWD/node_modules/.bin/:$PATH"
+    alias run='npm run'
+    alias scripts='jq ".scripts" package.json'
+  '';
+
+  # Environment variables
+  TOP_SECRET_KEY="ieu5nvtu2a;mtq9ut"; 
+}
