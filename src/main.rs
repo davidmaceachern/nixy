8760023c064388c230833b3eb7abbf1ccf2f9f08
David MacEachern <david.maceachern.tech@gmail.com>
2021-02-09 17:11:43 +0100

    Attempt to read yaml into map.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
+// #![forbid(unsafe_code)]
+// #![deny(missing_debug_implementations, nonstandard_style)]
+// #![warn(future_incompatible, rust_2018_idioms)]
+// #![warn(missing_docs)]
+
 mod nixy;
 
 use nixy::Nixy;
 use structopt::StructOpt;
 
 fn main() -> anyhow::Result<()> {
-    let args = Star::from_args();
+    let args = Nixy::from_args();
     nixy::run(
         args,
     )
--- src/nixy.rs
+++ src/nixy.rs
@@ -1,25 +1,50 @@
 use anyhow::{anyhow, bail};
 use structopt::StructOpt;
 use serde_yaml;
+use std::collections::BTreeMap;
+
+#[derive(PartialEq, Debug)]
+enum GenerateStatus {
+    Generated,
+    Printed,
+}
 
 #[derive(StructOpt, Debug)]
 pub struct Nixy {
+    /// Undo the previous action 
+    #[structopt(short, long)]
+    undo: bool,
     /// Generate a template for you to fill in 
     #[structopt(short, long)]
     generate: bool,
-    /// Run without actually updating account
+    /// Run without actually updating anything
     #[structopt(short, long)]
     dryrun: bool,
 }
 
+// #[derive(Debug, PartialEq, Default)]
+// struct NixTemplate {
+//     tools: Vec
+// }
+
 pub fn generate_template() {
+    // let nixTemplate: &'static str = "the quick brown fox jumps over the lazy dog";
+
+    // let buildInput = format!("pkgs.{}");
+
+    // let a = "AAA";
+    // let b = format!("BBB {}", a);
     unimplemented!()
 }
 
-pub fn run(api_key: String, args: Star) -> anyhow::Result<()> {
+pub fn run(api_key: String, args: Nixy) -> anyhow::Result<()> {
     let f = std::fs::File::open("nixy.yaml")?;
     let d: String = serde_yaml::from_reader(f)?;
-    let Nixy { generate, dryrun } = args;
+    let deserialized_map: BTreeMap<String, f64> = serde_yaml::from_str(&d)?;
+    println!("{:?}", deserialized_map);
+
+    let Nixy { generate, dryrun, undo } = args;
+
     if generate {
         let template_file_name = "nixy.yaml";
         println!("Generating a template for you to start with in {}", template_file_name);
@@ -28,6 +53,7 @@ pub fn run(api_key: String, args: Star) -> anyhow::Result<()> {
     if dryrun {
         println!("You enabled the dryrun flag.");
     }
+    
     Ok(())
 }
 
